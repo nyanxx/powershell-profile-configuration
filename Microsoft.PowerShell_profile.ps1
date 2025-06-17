3b27ad72b8aebf14e5e8142831f9132cb754d884
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-06-17 16:48:06 +0530

    add: prompt function to show battery percentage

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,30 @@
 # PowerShell Version v7.5.0
 
+Function Prompt {
+	$CurrentDir = Get-Location
+	$BatteryPercent = (Get-CimInstance Win32_Battery).EstimatedChargeRemaining
+
+	# Define ANSI color codes
+	$Green = "`e[32m"
+	$Orange = "`e[33m"
+	$Red = "`e[31m"
+	$Warning = "`e[31m⚠"
+	$ResetColor = "`e[0m"
+
+	if ($BatteryPercent -gt 50) {
+		$ColoredMsg = "$Green[$BatteryPercent%]"
+	} elseif ($BatteryPercent -gt 20) {
+		$ColoredMsg = "$Orange[$BatteryPercent%]"
+	} elseif ($BatteryPercent -gt 10) {
+		$ColoredMsg = "$Red[$BatteryPercent%]"
+	} else {
+		$ColoredMsg = "$Warning[$BatteryPercent%]"
+	}
+
+	return "$ColoredMsg $ResetColor PS $CurrentDir> "
+}
+
+
 # To Turn Off PowerShell Update Check 
 # Create an Environmental Variable "POWERSHELL_UPDATECHECK" and "Off" as Its Value.
 
