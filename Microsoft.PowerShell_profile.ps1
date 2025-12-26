23421fe861b7801f7a4f1865fe6403b4c2d047e9
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-12-26 18:49:58 +0530

    feat: update PowerShell Prompt function looks (style)
    - Taken and modified from Kali Linux's pwsh shell
    - Shows battery percentage as before

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,69 @@
 # PowerShell Version v7.5.0
 
 Function Prompt {
+	# The following block is surrounded by two delimiters.
+	# These delimiters must not be modified. Thanks.
+	# START KALI CONFIG VARIABLES
+	$PROMPT_ALTERNATIVE = 'twoline'
+	$NEWLINE_BEFORE_PROMPT = 'yes'
+	# STOP KALI CONFIG VARIABLES
 	#$HomeDir = [Environment]::GetFolderPath("UserProfile")
 	$CurrentDir = (Get-Location).Path
 	$BatteryPercent = (Get-CimInstance Win32_Battery).EstimatedChargeRemaining
 
 	if ($CurrentDir -eq "$HOME") {
 		$CurrentDir = $CurrentDir -replace [Regex]::Escape($HOME), '~\'
-	} elseif ($CurrentDir -like "$HOME*") {
+	}
+	elseif ($CurrentDir -like "$HOME*") {
 		$CurrentDir = $CurrentDir -replace [Regex]::Escape($HOME), '~'
 	}
-	
+
 	# Define ANSI color codes
 	#$Green = "e[32m"
 	#$Orange = "e[33m"
 	#$Red = "e[31m"
 	$RedWarning = "`e[31m⚠︎"
-	$ResetColor = "`e[0m"
+	# $ResetColor = "`e[0m"
 
 	if ($BatteryPercent -gt 50) {
 		$ColoredMsg = "`e[32m[$BatteryPercent%]"
-	} elseif ($BatteryPercent -gt 20) {
+	}
+	elseif ($BatteryPercent -gt 20) {
 		$ColoredMsg = "`e[33m[$BatteryPercent%]"
-	} elseif ($BatteryPercent -gt 10) {
+	}
+	elseif ($BatteryPercent -gt 10) {
 		$ColoredMsg = "`e[31m[$BatteryPercent%]"
-	} else {
+	}
+	else {
 		$ColoredMsg = "$RedWarning[$BatteryPercent%]"
 	}
-	
-	#return "$ColoredMsg$ResetColor $CurrentDir>"
-	return "$ColoredMsg$ResetColor $CurrentDir> "
+
+	$esc = [char]27
+	# $bell = [char]7
+	$bold = "$esc[1m"
+	$reset = "$esc[0m"
+	if ($NEWLINE_BEFORE_PROMPT -eq 'yes') { Write-Host }
+	if ($PROMPT_ALTERNATIVE -eq 'twoline') {
+		Write-Host "┌──(" -NoNewLine -ForegroundColor Blue
+		Write-Host "${bold}$([environment]::username)@$([system.environment]::MachineName)${reset}" -NoNewLine -ForegroundColor Magenta
+		Write-Host ")-[" -NoNewLine -ForegroundColor Blue
+		Write-Host "${bold}$($CurrentDir)${reset}" -NoNewLine -ForegroundColor White
+		Write-Host "]" -ForegroundColor Blue
+		Write-Host "└─" -NoNewLine -ForegroundColor Blue
+		Write-Host "${bold}$ColoredMsg${reset}" -NoNewline
+		Write-Host "${bold}>${reset}" -NoNewLine -ForegroundColor Blue
+	}
+	else {
+		Write-Host "${bold}PS " -NoNewLine -ForegroundColor Blue
+		Write-Host "$([environment]::username)@$([system.environment]::MachineName) " -NoNewLine -ForegroundColor Magenta
+		Write-Host "$($CurrentDir)>${reset}" -NoNewLine -ForegroundColor Blue
+	}
+
+	# return "$ColoredMsg$ResetColor $CurrentDir>"
+	# return "$ColoredMsg$ResetColor $CurrentDir> "
+	# Terminal title
+	# Write-Host "${esc}]0;PS>$([environment]::username)@$([system.environment]::MachineName): $($CurrentDir)${bell}" -NoNewLine
+	return " "
 }
 
 
@@ -59,7 +93,7 @@ if (Test-Path "$PROFILE_DIR\Functions\init.ps1") { . "$PROFILE_DIR\Functions\ini
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+	Import-Module "$ChocolateyProfile"
 }
 
 
